c536852bf33867b849175213ee27b8bfe82ea341
Roman Cheplyaka <roma@ro-che.info>
2013-01-25 10:00:46 +0200

    Use test-framework

--- test/test.hs
+++ test/test.hs
@@ -1,26 +1,18 @@
 {-# LANGUAGE BangPatterns, ScopedTypeVariables, FlexibleContexts #-}
-{-
 import Test.Framework
-import Test.Framework.HUnit
-import Test.Framework.SmallCheck
--}
+import Test.Framework.Providers.SmallCheck
 import Test.SmallCheck
 import Test.SmallCheck.Property
 import Test.SmallCheck.Series
-import Test.SmallCheck.Monad
+--import Test.SmallCheck.Monad
 import Control.Monad.Logic
 import Data.Maybe
 import Control.Monad.Identity
 import Data.Proxy
+import Data.List
 
 count :: Depth -> SC Identity a -> Integer
-count d a = fromMaybe 0 $ fst $ runIdentity $ runSC d $ go 0 a
-  where
-  go !acc a =
-    msplit a >>=
-    \r -> case r of
-      Just (_, rest) -> go (acc+1) rest
-      Nothing -> return acc
+count d a = genericLength $ list d a
 
 prop_size
   :: forall a m . (SizeTest a, Monad m)
@@ -49,3 +41,17 @@ instance SizeTest a => SizeTest [a] where
   size p d = 1 + size (Proxy :: Proxy a) (d-1) * size p (d-1)
 
 instance (SizeTest a, SizeTest b) => SizeTest (a -> b)
+
+------------------------------
+-- Actual testing
+------------------------------
+
+main = defaultMain [sizeTests]
+
+sizeTests = testGroup "Size tests"
+  [ testProperty "Bool"       $ prop_size (Proxy :: Proxy Bool)
+  , testProperty "Int"        $ prop_size (Proxy :: Proxy Int)
+  , testProperty "Integer"    $ prop_size (Proxy :: Proxy Integer)
+  , testProperty "Maybe Int"  $ prop_size (Proxy :: Proxy (Maybe Int))
+  , testProperty "[Int]"      $ prop_size (Proxy :: Proxy [Int])
+  ]
