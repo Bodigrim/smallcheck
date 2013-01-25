5f81fae8aacea84b89cfd3696c5be23abc595cca
Roman Cheplyaka <roma@ro-che.info>
2013-01-25 11:11:59 +0200

    test/run.sh

---
+++ test/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eu
+
+cabal_install () {
+  cabal install --force-reinstalls --ghc-options=-fhpc --disable-library-profiling --disable-documentation
+}
+
+if [ "${1:-""}" = '--deps' ]
+then
+  (cd .. && cabal_install)
+  (cd ../test-framework-smallcheck && cabal_install)
+fi
+
+ghc -fforce-recomp -fhpc test.hs
+./test
+hpc markup --srcdir=. --srcdir=../ --srcdir=../test-framework-smallcheck --destdir=html test.tix
