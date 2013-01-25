9f7277e3168ad609afef37f962a41be1fecfd7a0
Roman Cheplyaka <roma@ro-che.info>
2013-01-25 10:16:01 +0200

    Reogranize tests

--- test/test.hs
+++ test/test.hs
@@ -1,18 +1,31 @@
-{-# LANGUAGE BangPatterns, ScopedTypeVariables, FlexibleContexts #-}
+{-# LANGUAGE BangPatterns, ScopedTypeVariables, FlexibleContexts,
+             ExistentialQuantification #-}
 import Test.Framework
 import Test.Framework.Providers.SmallCheck
 import Test.SmallCheck
 import Test.SmallCheck.Property
 import Test.SmallCheck.Series
---import Test.SmallCheck.Monad
 import Control.Monad.Logic
 import Data.Maybe
 import Control.Monad.Identity
 import Data.Proxy
 import Data.List
 
+------------------------------
+-- Auxiliary definitions
+------------------------------
+
+class Serial Identity a => SizeTest a where
+  size :: Proxy a -> Integer -> Integer
+
+data TestableType = forall a . SizeTest a => TestableType String (Proxy a)
+
 count :: Depth -> SC Identity a -> Integer
 count d a = genericLength $ list d a
+
+------------------------------
+-- Kinds of tests
+------------------------------
 
 prop_size
   :: forall a m . (SizeTest a, Monad m)
@@ -21,8 +34,12 @@ prop_size proxy = property $
   \d ->
     count d (series :: SC Identity a) == size proxy (fromIntegral d)
 
-class Serial Identity a => SizeTest a where
-  size :: Proxy a -> Integer -> Integer
+test_size :: TestableType -> Test
+test_size (TestableType name p) = testProperty name $ prop_size p
+
+------------------------------
+-- SizeTest instances
+------------------------------
 
 instance SizeTest Bool where
   size _ d = 2
@@ -43,15 +60,22 @@ instance SizeTest a => SizeTest [a] where
 instance (SizeTest a, SizeTest b) => SizeTest (a -> b)
 
 ------------------------------
+-- Testable types
+------------------------------
+
+types =
+  [ TestableType "Bool"       (Proxy :: Proxy Bool)
+  , TestableType "Bool"       (Proxy :: Proxy Bool)
+  , TestableType "Int"        (Proxy :: Proxy Int)
+  , TestableType "Integer"    (Proxy :: Proxy Integer)
+  , TestableType "Maybe Int"  (Proxy :: Proxy (Maybe Int))
+  , TestableType "[Int]"      (Proxy :: Proxy [Int])
+  ]
+
+------------------------------
 -- Actual testing
 ------------------------------
 
 main = defaultMain [sizeTests]
 
-sizeTests = testGroup "Size tests"
-  [ testProperty "Bool"       $ prop_size (Proxy :: Proxy Bool)
-  , testProperty "Int"        $ prop_size (Proxy :: Proxy Int)
-  , testProperty "Integer"    $ prop_size (Proxy :: Proxy Integer)
-  , testProperty "Maybe Int"  $ prop_size (Proxy :: Proxy (Maybe Int))
-  , testProperty "[Int]"      $ prop_size (Proxy :: Proxy [Int])
-  ]
+sizeTests = testGroup "Size tests" $ map test_size types
